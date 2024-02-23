e5639bac39c7928f3d87b39391a9aab952b242e2
Nathan <nathan.manera@gmail.com>
2024-02-23 10:28:59 -0300

    Setup do projeto e criação das classes da camada de persistência (model)

---
+++ src/main/kotlin/_projetos/creditapplicationsystem/repository/CustomerRepository.kt
@@ -0,0 +1,8 @@
+package _projetos.creditapplicationsystem.repository
+
+import _projetos.creditapplicationsystem.entity.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository: JpaRepository<Customer, Long>

5fb6cf0f406908daf0abd86586bcd3eb360c3a79
Nathan <nathan.manera@gmail.com>
2024-03-05 12:17:31 -0300

    Teste Unitário: CreditService - save

---
+++ src/test/kotlin/_projetos/creditapplicationsystem/service/CreditServiceTest.kt
@@ -0,0 +1,92 @@
+package _projetos.creditapplicationsystem.service
+
+import _projetos.creditapplicationsystem.entity.Credit
+import _projetos.creditapplicationsystem.entity.Customer
+import _projetos.creditapplicationsystem.exception.BusinessException
+import _projetos.creditapplicationsystem.repository.CreditRepository
+import _projetos.creditapplicationsystem.service.impl.CreditService
+import _projetos.creditapplicationsystem.service.impl.CustomerService
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.unmockkAll
+import io.mockk.verify
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
+import java.time.LocalDate
+
+@ExtendWith(MockKExtension::class)
+class CreditServiceTest {
+    @MockK
+    lateinit var creditRepository: CreditRepository
+
+    @MockK
+    lateinit var customerService: CustomerService
+
+    @InjectMockKs
+    lateinit var creditService: CreditService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        //creditService = CreditService(creditRepository, customerService)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should create credit `() {
+        //given
+        val credit: Credit = buildCredit()
+        val customerId: Long = 1L
+
+        every { customerService.findById(customerId) } returns credit.customer!!
+        every { creditRepository.save(credit) } returns credit
+        //when
+        val actual: Credit = this.creditService.save(credit)
+        //then
+        verify { customerService.findById(customerId) }
+        verify { creditRepository.save(credit) }
+
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(credit)
+    }
+
+    @Test
+    fun `should not create credit when invalid day first installment`() {
+        //given
+        val invalidDayFirstInstallment: LocalDate = LocalDate.now().plusMonths(5)
+        val credit: Credit = buildCredit(dayFirstInstallment = invalidDayFirstInstallment)
+
+        every { creditRepository.save(credit) } answers { credit }
+        //when
+        Assertions.assertThatThrownBy { creditService.save(credit) }
+            .isInstanceOf(BusinessException::class.java)
+            .hasMessage("First Installment must be a maximum of 3 months after the current day")
+        //then
+        verify(exactly = 0) { creditRepository.save(any()) }
+    }
+
+    companion object {
+        private fun buildCredit(
+            creditValue: BigDecimal = BigDecimal.valueOf(100.0),
+            dayFirstInstallment: LocalDate = LocalDate.now().plusMonths(2L),
+            numberOfInstallments: Int = 15,
+            customer: Customer = CustomerServiceTest.buildCustomer()
+        ): Credit = Credit(
+            creditValue = creditValue,
+            dayFirstInstallment = dayFirstInstallment,
+            numberOfInstallments = numberOfInstallments,
+            customer = customer
+        )
+    }
+}
